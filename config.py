104b77f2677a29050c17b6c533997e7e9f210316
teopozzii <matteopozzi00@gmail.com>
2026-01-30 21:21:23 +0100

    Draft item categorization

---
+++ config.py
@@ -0,0 +1,5 @@
+default_categories = {
+    'Medici': ["dentistico", "medical", "daho", "galeazzi"],
+    "Utenze" : ["enel energia", "addebito utenze", "vodafone", 
+                                 "segnoverde", "sky"],
+}
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import psutil
 import pandas as pd
 import re
 from pathlib import Path
+from config import default_categories
 
 @magics_class
 class TrafficMagic(Magics):
@@ -27,13 +28,15 @@ def register_traffic_magic():
 
 
 class Movimenti:
-    def __init__(self):
+    def __init__(self, categories=default_categories):
         os.system('cls' if os.name == 'nt' else 'clear')
         os.system('mkdir -p ~/code/conto/data')
         self.data_dir = Path("~/code/conto/data").expanduser()
-        self.data = self.load_excel_movimenti()
+        self.data = self.reset_table()
+        self.categories = categories
 
     def load_excel_movimenti(self):
+        self.data = None
         name_pattern = r'MovimentiCC_\d{4}-\d{2}-\d{2}\.xlsx'
         files = []
         for file in self.data_dir.iterdir():
@@ -45,3 +48,33 @@ class Movimenti:
             return None
         df = pd.read_excel(files[0], header=None)
         return df
+
+    def reset_table(self, flag = "Data contabile"):
+        self.data = self.load_excel_movimenti()
+        col_header_limit, row_header_limit = 10, 10
+    
+        headers_area = self.data.iloc[:col_header_limit, :row_header_limit]
+        if flag not in headers_area.values:
+            raise ValueError("Unidentifiable headers.")
+        col_headers_index, row_headers_index = (
+            (headers_cell := headers_area.isin([flag])).any(axis=0).idxmax(), headers_cell.any(axis=1).idxmax()
+        )
+        self.data.columns = self.data.iloc[row_headers_index, :].values
+        self.data = self.data.iloc[
+            row_headers_index + 1:,
+            col_headers_index:
+            ].reset_index(drop=True)
+        return self.data
+
+    def categorize_movimenti(self, col_name='Descrizione'):
+        if col_name not in self.data.columns:
+            raise ValueError(f"'{col_name}' column not found in data.")
+    
+        def categorize_row(description):
+            for category, keywords in self.categories.items():
+                if any(keyword.lower() in str(description).lower() for keyword in keywords):
+                    return category
+            return 'Uncategorized'
+        
+        self.data['Categoria'] = self.data['Descrizione'].apply(categorize_row)
+        return self.data
