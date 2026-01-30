949d844e7fc25e19ef0ea607ecf60c71eb258efc
teopozzii <matteopozzi00@gmail.com>
2026-01-31 00:53:42 +0100

    Make categorization more complete and consistent

--- utils/bankstatement.py
+++ utils/bankstatement.py
@@ -2,15 +2,18 @@ import os
 import pandas as pd
 import re
 from pathlib import Path
-from .config import default_categories
+import json
+with open(Path("~/code/conto/utils/config.json").expanduser(), 'r') as f:
+    config = json.load(f)
 
 class BankStatement:
-    def __init__(self, categories=default_categories):
+    def __init__(self, owner="papà", categories=None):
         os.system('cls' if os.name == 'nt' else 'clear')
         os.system('mkdir -p ~/code/conto/data')
         self.data_dir = Path("~/code/conto/data").expanduser()
+        self.headers = config[owner]["headers"]
         self.data = self.reset_table()
-        self.categories = categories
+        self.categories = categories if categories else config[owner]["default_categories"]
 
     def load_excel_statement(self):
         self.data = None
@@ -26,8 +29,9 @@ class BankStatement:
         df = pd.read_excel(files[0], header=None)
         return df
 
-    def reset_table(self, flag = "Data contabile"):
+    def reset_table(self):
         self.data = self.load_excel_statement()
+        flag = self.headers["loc_identif"]
         col_header_limit, row_header_limit = 10, 10
     
         headers_area = self.data.iloc[:col_header_limit, :row_header_limit]
@@ -41,13 +45,15 @@ class BankStatement:
             row_headers_index + 1:,
             col_headers_index:
             ].reset_index(drop=True)
-        self.data["Data valuta"] = pd.to_datetime(self.data["Data valuta"], format="%d/%m/%Y")
-        self.data["Importo"] = pd.to_numeric(self.data["Importo"].astype(str).str.replace(',', '.'), errors='coerce')
+        self.data[self.headers["date"]] = pd.to_datetime(self.data[self.headers["date"]], format="%d/%m/%Y")
+        self.data[self.headers["value"]] = pd.to_numeric(self.data[self.headers["value"]].astype(str).str.replace(',', '.'), errors='coerce')
         return self.data
 
-    def categorize_expenses(self, col_name='Descrizione'):
-        if col_name not in self.data.columns:
-            raise ValueError(f"'{col_name}' column not found in data.")
+    def categorize_expenses(self):
+        description_col = self.headers.get("descript", "Descrizione")
+        category_col = self.headers.get("category", "Categoria")
+        if description_col not in self.data.columns:
+            raise ValueError(f"'{description_col}' column not found in data.")
     
         def categorize_row(description):
             for category, keywords in self.categories.items():
@@ -55,5 +61,5 @@ class BankStatement:
                     return category
             return 'Uncategorized'
         
-        self.data['Categoria'] = self.data['Descrizione'].apply(categorize_row)
+        self.data[category_col] = self.data[description_col].apply(categorize_row)
         return self.data
--- utils/config.py
+++
@@ -1,5 +0,0 @@
-default_categories = {
-    'Medici': ["dentistico", "medical", "daho", "galeazzi"],
-    "Utenze" : ["enel energia", "addebito utenze", "vodafone", 
-                                 "segnoverde", "sky"],
-}
